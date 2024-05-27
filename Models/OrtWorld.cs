a0a82d9555dd61c1a067a97c1f8e3e26e83dfc7b
URIEL ENGELBERG <48796259@ort>
2024-05-27 09:03:35 -0300

    Empezando el tp04

---
+++ Models/OrtWorld.cs
@@ -0,0 +1,14 @@
+public class OrtWorld{
+   public static List<string> ListaDestinos {get; private set;}
+   public static List<string> ListaHoteles {get; private set;}
+   public static List<string> ListaAereos {get; private set;}
+   public static List<string> ListaExcursiones {get; private set;}
+   public static Dictionary<string, Paquete> Paquetes {get; private set;}
+
+    static bool IngresarPaquete(string destinoSeleccionado, Paquete paquete){
+        
+        
+        return false;
+    }
+
+}
